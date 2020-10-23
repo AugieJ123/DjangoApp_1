f5c2a6e92f891be34960f2d9180943c128d7de6d
AugieJ123 <53660338+AugieJ123@users.noreply.github.com>
2020-10-23 00:10:57 +0000

    performing query

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Import all the models for querying...
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    # Querying all the orders and customers in the dashboard
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    # Counting all the orders and customers
+    total_customers = customers.count()
+    total_orders = orders.count()
+
+    # Orders that are delivered
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {
+        'orders': orders, 
+        'customers': customers, 
+        'total_customers': total_customers, 
+        'total_orders': total_orders, 
+        'delivered': delivered, 
+        'pending': pending
+        }
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    # Query all the products in the database...
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
