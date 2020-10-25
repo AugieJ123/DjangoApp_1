f1643d48366cc546eec41b3cb60bbe0866cb0b1d
AugieJ123 <53660338+AugieJ123@users.noreply.github.com>
2020-10-25 13:40:36 +0000

    building the update and delete page

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 # Import all the models for querying...
 from .models import *
+# Importing the form class to create form
+from .form import OrderForm
 
 # Create your views here.
 def home(request):
@@ -44,3 +46,39 @@ def customer(request, pk):
 
     context = {'customer': customer, 'orders': orders, 'order_count': order_count}
     return render(request, 'accounts/customer.html', context)
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        #print('Printing POST:', request.POST )
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'accounts/order_form.html', context)
+
+def updateOrder(request, pk):
+    # Query the order to update
+    order = Order.objects.get(id=pk)
+
+    form = OrderForm(instance=order)
+    if request.method == 'POST':
+        #print('Printing POST:', request.POST )
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form,}
+    return render(request, 'accounts/order_form.html', context)
+
+def deleteOrder(request,pk=id):
+    order = get_object_or_404(Order, pk=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+
+    context = {'item': order}
+    return render(request, 'accounts/delete.html', context)
