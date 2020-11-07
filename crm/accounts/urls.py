96d042f802e39efe3b26ba4728a29871490e50e6
AugieJ123 <53660338+AugieJ123@users.noreply.github.com>
2020-11-07 16:20:43 +0000

    User Permission

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('register/', views.registerPage, name='register'),
+    path('login/', views.loginPage, name='login'),
+    path('logout/', views.logoutUser, name='logout'),
 
-    path('register/', views.registerPage, name = 'register'),
-    path('login/', views.loginPage, name = 'login'),
-    path('logout/', views.logoutUser, name = 'logout'),
-
-    path('products/', views.products, name = 'product'),
-    path('customer/<str:pk>', views.customer, name = 'customer'),
+    path('', views.home, name="home"),
+    path('user/', views.userPage, name="user-page"),
+    path('products/', views.products, name='product'),
+    path('customer/<str:pk>', views.customer, name='customer'),
 
     path('create_order/<str:pk>', views.createOrder, name='create_order'),
     path('update_order/<str:pk>', views.updateOrder, name='update_order'),
